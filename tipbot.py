34589de627bb0ff31a1213282320d061dfb6e2d2
tomgamer <tomanhorak22@gmail.com>
2024-10-17 22:52:49 +0200

    funkční ?

--- tipbot.py
+++ tipbot.py
@@ -3,6 +3,7 @@ import json
 import os
 import time
 from dotenv import load_dotenv
+import youtube_dl
 
 load_dotenv()
 bot = discord.Bot()
@@ -100,7 +101,112 @@ async def wolf(ctx: discord.ApplicationContext, member: discord.Member, times: i
 async def slovnik(ctx: discord.ApplicationContext):
         await ctx.respond("The Classics: Skibidi, Grimace Shake, Fanum Tax, Doomscrolling, Chat, Kai Cenat, Edging, Rizzing, Gooning, Rizz, Rizzing, Sigma, Only in Ohio, 19 Dollar Fortnite card, Double Pump, Hitting the Griddy, Ice Spice, Level 100 Gyatt, Gooning cave, Discord Kitten, You make my heart burn\n Adjectives: Sus, Sussy, Sussy baka, Rizzler, Alpha, Beta, Sigma, Based, Chuds, Thot, Skibidi Sigma, Simp, Soyboy, Chads, Sturdy, Gamer, Incel, Cringe/Cringey, Furry, Discord Kitten, Streamer, Zesty, Boomer, Doomer, Coomer, Zoomer, Gooner, Goofy, Silly, Cracked at fortnite\n Brands/Franchises: Fortnite, Reddit, Youtube Shorts, TikTok, Mc Donalds, Gucci, Supreme, Discord, Roblox, Subway Surfers shorts, Family guy shorts, Tinder, Uber Eats,\n Streamers/People: Mr beast, Ice Spice, Kai cenat, Fanum, Caseoh, DaFuqBoom, Baby gronk, Master Oogway, Peter Griffin, Raven Team leader, Andrew tate, Ben Shapiro, Jordan Peterson, Elon Musk, Dababy, Lebron James, Kanye West.\n Sounds: Yeet, Oof, Leroy Jenkins, gyatt, Happy Happy Happy, Oh no!!\n Catchphrases: Leroy Jenkins, Hell naw, What the dog doin?, imma head out, Upgrades, people, more upgrades, Zoo wee mama, Deez Nuts, Shikanoko nokonoko Koshitantan, Only a spoonful, POV:, IS THAT A JOJO REFERENCE???, How bad can i be?, Why do i hear boss music, Aw shit here we go again, No, you are not a gamer\n Still water + adrenaline + noradrenaline + hawk tuah + anger issues + balkan parents + english or Spanish + german stare + Balkan rage + jonkler laugh + phonk + those who know=\n Ksi music + lunchly + winter arc + still water baths + nonadrenaline + flow state + MANGO MANGO phonk + Prime + Balkan\n skibidi gyatt rizz only in ohio duke dennis did you pray today livvy dunne rizzing up baby gronk sussy imposter pibby glitch in real life sigma alpha omega male grindset andrew tate goon cave freddy fazbear colleen ballinger smurf cat vs strawberry elephant blud dawg shmlawg ishowspeed a whole bunch of turbulence ambatukam bro really thinks he s carti literally hitting the griddy the ocky way kai cenat fanum tax garten of banban no edging in class not the mosquito again bussing axel in harlem whopper whopper whopper whopper 1 2 buckle my shoe goofy ahh aiden ross sin city monday left me broken quirked up white boy busting it down sexual style goated with the sauce john pork grimace shake kiki do you love me huggy wuggy nathaniel b lightskin stare biggest bird omar the referee amogus uncanny wholesome reddit chungus keanu reeves pizza tower zesty poggers kumalala savesta quandale dingle glizzy rose toy ankha zone thug shaker morbin time dj khaled sisyphus oceangate shadow wizard money gang ayo the pizza here PLUH nair butthole waxing t-pose ugandan knuckles family guy funny moments compilation with subway surfers gameplay at the bottom nickeh30 ratio uwu delulu opium bird cg5 mewing fortnite battle pass all my fellas gta 6 backrooms gigachad based cringe kino redpilled no nut november pokénut november foot fetish F in the chat i love lean looksmaxxing gassy social credit bing chilling xbox live mrbeast kid named finger better caul saul i am a surgeon hit or miss i guess they never miss huh i like ya cut g ice spice gooning fr we go gym kevin james josh hutcherson coffin of andy and leyley metal pipe falling")
 
+music_queue = []
+voice_client = None
 
+# YouTube download options
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # Bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+# Join the voice channel
+async def ensure_voice(ctx):
+    global voice_client
+    if not ctx.author.voice:
+        await ctx.respond("You need to be in a voice channel first!")
+        return False
+    channel = ctx.author.voice.channel
+    if voice_client is None or not voice_client.is_connected():
+        voice_client = await channel.connect()
+    return True
+
+
+# Play music
+async def play_next(ctx):
+    if len(music_queue) > 0:
+        url = music_queue.pop(0)
+        data = ytdl.extract_info(url, download=False)
+        url2 = data['url']
+        voice_client.play(discord.FFmpegPCMAudio(url2, **ffmpeg_options))
+        await ctx.send(f"Now playing: {data['title']}")
+    else:
+        await ctx.send("No more songs in the queue!")
+
+
+
+@bot.slash_command(name="music")
+async def music(ctx: discord.ApplicationContext):
+    # Create options for users to choose from
+    options = [
+        discord.SelectOption(label="Play", description="Play a song"),
+        discord.SelectOption(label="Pause", description="Pause the current song"),
+        discord.SelectOption(label="Skip", description="Skip the current song"),
+        discord.SelectOption(label="Stop", description="Stop the music and leave")
+    ]
+
+    select_menu = discord.ui.Select(placeholder="Choose a music command...", options=options)
+
+    async def select_callback(interaction):
+        choice = select_menu.values[0]
+
+        if choice == "Play":
+            await ctx.send("Please provide a YouTube URL to play.")
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            try:
+                msg = await bot.wait_for('message', check=check, timeout=30)
+                music_queue.append(msg.content)
+                await ctx.send(f"Added to queue: {msg.content}")
+                if not voice_client or not voice_client.is_playing():
+                    await play_next(ctx)
+            except Exception as e:
+                await ctx.send("Error: Could not add song.")
+
+        elif choice == "Pause":
+            if voice_client and voice_client.is_playing():
+                voice_client.pause()
+                await ctx.send("Music paused.")
+            else:
+                await ctx.send("No music is currently playing.")
+
+        elif choice == "Skip":
+            if voice_client and voice_client.is_playing():
+                voice_client.stop()
+                await play_next(ctx)
+            else:
+                await ctx.send("No music is currently playing.")
+
+        elif choice == "Stop":
+            if voice_client:
+                await voice_client.disconnect()
+                await ctx.send("Music stopped and bot left the voice channel.")
+            else:
+                await ctx.send("Bot is not in a voice channel.")
+
+    select_menu.callback = select_callback
+    view = discord.ui.View()
+    view.add_item(select_menu)
+    await ctx.send("Select an option:", view=view)
 
 # SUPER SIGMA: Kick from another server's voice channel
 @bot.slash_command(name="super_sigma", guild=discord.Object(id=876802324192952320))
@@ -140,7 +246,6 @@ async def on_ready():
     activity = discord.Activity(type=discord.ActivityType.playing, name="with honík")
     await bot.change_presence(activity=activity)
     load_scores()
-    await bot.load_extension("music_cog")
     await bot.sync_commands()
     print(f"{bot.user} is online!")
 
