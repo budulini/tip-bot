50e5598f7791c272bcabcc7efadbe7af2fe6e1bb
tomgamer <tomanhorak22@gmail.com>
2024-10-18 16:28:16 +0200

    Brain rot added

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import tipbot
-if __name__ =='__main__':
-    tipbot.runBot()
+import youtube_dl
+
+url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # Replace with the YouTube URL
+
+ydl_opts = {'format': 'bestaudio'}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(url, download=False)
+    print(info)
--- musicshit.py
+++ musicshit.py
@@ -1,4 +1,9 @@
+import youtube_dl
 
+url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # Replace with the YouTube URL
 
-# Initialize the music player
+ydl_opts = {'format': 'bestaudio'}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(url, download=False)
+    print(info)
 
