b075149678e7ce7df7478103aea7c9cccf94aa36
tomgamer <tomanhorak22@gmail.com>
2024-10-18 22:14:43 +0200

    fix
    sem kkt a přepsal sem main.py omylem

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import youtube_dl
+import tipbot
 
-url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # Replace with the YouTube URL
-
-ydl_opts = {'format': 'bestaudio'}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    info = ydl.extract_info(url, download=False)
-    print(info)
+if __name__ == "__main__":
+    tipbot.runBot()
